95189076dbabe26ad76b8cfbc983cc21fa37f750
Dinu Blanovschi <git@dnbln.dev>
2025-05-10 20:26:40 +0200

    feat: continue with nix config

---
+++ nix-darwin-config/hosts/nebula/configuration.nix
@@ -0,0 +1,51 @@
+{ pkgs, inputs, configurationRevision, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages =
+    [ pkgs.vim
+      pkgs.neovim
+      pkgs.git
+    ];
+
+  # Necessary for using flakes on this system.
+  nix.enable = false;
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Enable alternative shell support in nix-darwin.
+  programs.zsh.enable = true;
+  # programs.fish.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system = {
+    inherit configurationRevision;
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    stateVersion = 6;
+    defaults = {
+      dock.autohide = true;
+      dock.mru-spaces = false;
+      finder.AppleShowAllExtensions = true;
+      finder.FXPreferredViewStyle = "clmv";
+      loginwindow.LoginwindowText = "dnbln";
+      screencapture.location = "~/Pictures/screenshots";
+      screensaver.askForPasswordDelay = 10;
+    };
+  };
+
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "x86_64-darwin";
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  users.users.dinu = {
+    name = "dinu";
+    home = "/Users/dinu";
+  };
+
+  home-manager.users = {
+    dinu = import ./home.nix;
+  };
+}
